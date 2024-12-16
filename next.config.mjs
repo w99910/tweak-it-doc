ce5f73ff43f0244934996ee68cf05d03a1c09e3c
Thomas <zawlintun@robust.best>
2024-12-16 16:19:59 +0700

    change static render

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 import nextra from 'nextra'
 
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true // mandatory, otherwise won't export
+    },
+    // Optional: Change the output directory `out` -> `dist`
+    distDir: "dist"
+}
+
 const withNextra = nextra({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.jsx'
 })
 
-export default withNextra()
+export default withNextra(nextConfig)
